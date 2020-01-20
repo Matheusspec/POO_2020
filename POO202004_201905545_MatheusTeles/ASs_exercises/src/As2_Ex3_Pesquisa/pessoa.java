6c561e447aebb77aeff6b174c9adc7899785657e
Matheusspec <matheusteles015@gmail.com>
2020-01-20 16:39:39 -0300

    As2

---
+++ POO202004_201905545_MatheusTeles/ASs_exercises/src/As2_Ex3_Pesquisa/pessoa.java
@@ -0,0 +1,9 @@
+package As2_Ex3_Pesquisa;
+
+public class pessoa {
+	String sexo;
+	String olho;
+	String cabelo;
+	int idade;
+
+}
---
+++ POO202004_201905545_MatheusTeles/ASs_exercises/src/As2_Ex5_familia/pessoa.java
@@ -0,0 +1,12 @@
+package As2_Ex5_familia;
+
+public class pessoa {
+	String nome;
+	int idade;
+	String pai;
+	String mae;
+	String vo;
+	String vo1;
+	String filho;
+
+}
