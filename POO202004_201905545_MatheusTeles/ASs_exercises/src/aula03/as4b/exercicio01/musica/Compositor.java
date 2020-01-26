63b23f009216839c0d0ea38ddfcc1134148638f4
Matheusspec <matheusteles015@gmail.com>
2020-01-26 00:32:50 -0300

    ASs

---
+++ POO202004_201905545_MatheusTeles/ASs_exercises/src/aula03/as4b/exercicio01/musica/Compositor.java
@@ -0,0 +1,29 @@
+package aula03.as4b.exercicio01.musica;
+
+public class Compositor {
+	private String nome;
+	private String nacionalidade;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public Compositor(String nome, String nacionalidade) {
+		this.nome = nome;
+		this.nacionalidade = nacionalidade;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getNacionalidade() {
+		return nacionalidade;
+	}
+	
+	public void setNacionalidade(String nacionalidade) {
+		this.nacionalidade = nacionalidade;
+	}
+	
+}
